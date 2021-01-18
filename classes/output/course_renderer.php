6f6504e626d6238d717a2884f9d86642245ccc37
Matthias Opitz <opitz@gmx.de>
2021-01-18 17:24:31 +0000

    initializing variable

--- classes/output/course_renderer.php
+++ classes/output/course_renderer.php
@@ -926,6 +926,7 @@ class qmultopics_course_renderer extends \core_course_renderer{
         global $COURSE;
 
         // Show attempts by enrolled students.
+        $badgetext = '';
         $badgeclass = '';
         $capability = 'quiz';
         $pretext = '';
