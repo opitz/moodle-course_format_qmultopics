329d0b9cd0b819096167e8a7d4955de5fb0bdb0f
Matthias Opitz <opitz@gmx.de>
2021-12-03 12:41:52 +0000

    QMPLUS-575: added support for replacment function for course_section_cm_completion() - 2

--- classes/output/course_renderer.php
+++ classes/output/course_renderer.php
@@ -209,11 +209,9 @@ class qmultopics_course_renderer extends \core_course_renderer{
 
         if (class_exists('\core_completion\activity_custom_completion')) {
             // Render the activity information.
-            $modinfo = get_fast_modinfo($this->course);
-            $cm = $modinfo->get_cm($this->cm->id);
-            $completiondetails = \core_completion\cm_completion_details::get_instance($cm, $USER->id);
-            $activitydates = \core\activity_dates::get_dates_for_module($cm, $USER->id);
-            $modicons .= $this->output->activity_information($cm, $completiondetails, $activitydates);
+            $completiondetails = \core_completion\cm_completion_details::get_instance($mod, $this->user->id);
+            $activitydates = \core\activity_dates::get_dates_for_module($mod, $this->user->id);
+            $modicons .= $this->output->activity_information($mod, $completiondetails, $activitydates);
         } else {
             $modicons .= $this->course_section_cm_completion($course, $completioninfo, $mod, $displayoptions);
         }
